07d021492ca5ee327ffd86bd1b3b7fa7594605fe
Firuz Afzalov <afzalov98@gmail.com>
2020-01-26 00:50:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,14 +31,7 @@ end
 
 def hit?(total)
   prompt_user
-  user_input = get_user_input
-  if user_input == "h"
-    total += deal_card
-    elsif user_input == "s"
-    total
-  else
-    invalid_command
-  end
+  
 end
 
 def invalid_command
